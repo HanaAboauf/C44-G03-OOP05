22b72ce6722df104f4a7a8e8fdf31eb95259fa42
Hana <hanamarwasadek2004@gmail.com>
2025-08-07 00:02:43 +0300

    solution of question01

---
+++ assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question01
+
+            //Circle circle = new Circle(5);
+            //circle.DisplayShapeInfo();
+            //Console.WriteLine("==================");
+            //Rectangle rectangle = new Rectangle(3, 5);
+            //rectangle.DisplayShapeInfo();
+            #endregion
+        }
+    }
+}
---
+++ assignment/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    internal class Rectangle : IRectangle
+    {
+
+    
+
+        public double Width {  get; set; }
+
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height) {
+            Width = width;
+            Height = height;
+        }
+        public double Area => Width*Height;
+
+        public void DisplayShapeInfo()
+        {
+            Console.WriteLine($"Rectangle with Width: {Width}, Height: {Height} Area: {Area}");
+        }
+    }
+}
