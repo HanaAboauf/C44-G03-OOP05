d65588d45357ea68f80c055f592cdb7560923462
Hana <hanamarwasadek2004@gmail.com>
2025-08-07 00:53:57 +0300

    solution of questions02, 03

---
+++ assignment/EmailNotificationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    internal class EmailNotificationService : INotificationService
+    {
+        public void SendNotification(string recipient, string message)
+        {
+            Console.WriteLine($"Sending Email to {recipient}: {message}");
+        }
+    }
+  
+}
--- assignment/Program.cs
+++ assignment/Program.cs
@@ -12,6 +12,36 @@
             //Rectangle rectangle = new Rectangle(3, 5);
             //rectangle.DisplayShapeInfo();
             #endregion
+
+            #region Question02
+
+
+            //IAuthenticationService authenticationService = new BasicAuthenticationService();
+
+            //Console.WriteLine(authenticationService.AuthenticateUser("admin", "password"));
+
+            //Console.WriteLine(authenticationService.AuthenticateUser("admin", "pasword"));
+
+            //Console.WriteLine(authenticationService.AuthorizeUser("admin", "Administrator"));
+
+            //Console.WriteLine(authenticationService.AuthorizeUser("admin", "user"));
+            #endregion
+
+            #region Question03
+
+            //INotificationService emailService = new EmailNotificationService();
+            //INotificationService smsService = new SmsNotificationService();
+            //INotificationService pushService = new PushNotificationService();
+
+            //string message = "Your verification code is 123456";
+
+            //emailService.SendNotification("hana.abuauf@example.com", message);
+            //smsService.SendNotification("+1097261273", message);
+            //pushService.SendNotification("device_token", message);
+
+            #endregion
+
+
         }
     }
 }
